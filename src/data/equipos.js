6b1e03c922bb9a10a73435744dec406bf0acff75
nicokrym <nicokrym@gmail.com>
2026-03-28 14:47:54 -0300

    Initial commit

---
+++ src/data/equipos.js
@@ -0,0 +1,14 @@
+export const teams = [
+  {
+    id: "chiefs",
+    name: "Kansas City Chiefs",
+    city: "Kansas City",
+    conference: "AFC",
+    division: "West",
+    colors: {
+      primary: "#E31837",
+      secondary: "#FFB81C",
+    },
+    logo: "https://a.espncdn.com/i/teamlogos/nfl/500/kc.png"
+  },
+];
